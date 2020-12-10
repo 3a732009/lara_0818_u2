c28a12049873c7941024c0e56a8db2411b1403fd
chang ting yu <s3a732009@student.ncut.edu.tw>
2020-12-10 23:24:01 +0800

    從HelloController 將參數傳遞至 view

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class HelloController extends Controller
 {
-    //
+    public function index($name ='Everybody')
+    {
+
+        $data=['name'=>$name];
+
+        return  view('hello.index',$data);
+    }
 }
